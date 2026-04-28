fe25e6fdb424aef4aad7ca245a60bec916f7ebe6
Jollofia19 <ejazabdullah2007@gmail.com>
2026-04-28 20:54:14 +0800

    Linked List completion

--- src/structures/linkedlist.cpp
+++ src/structures/linkedlist.cpp
@@ -1,3 +1,45 @@
 #include "linkedlist.h"
 #include "node.h"
-#include "resident.h"
+#include "linkedlist.h"
+
+// Constructor
+LinkedList::LinkedList() {
+    head = nullptr;
+}
+
+// Insert at end
+void LinkedList::insertEnd(Resident r) {
+    Node* newNode = new Node;
+    newNode->data = r;
+    newNode->next = nullptr;
+
+    if (head == nullptr) {
+        head = newNode;
+    } else {
+        Node* current = head;
+
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+
+        current->next = newNode;
+    }
+}
+
+// Return head pointer
+Node* LinkedList::getHead() {
+    return head;
+}
+
+// Destructor
+LinkedList::~LinkedList() {
+    Node* current = head;
+
+    while (current != nullptr) {
+        Node* temp = current;
+        current = current->next;
+        delete temp;
+    }
+
+    head = nullptr;
+}
